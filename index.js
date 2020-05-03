99f181f7c1280a09ac70a2145d94c0d0bd77fc4c
Abbie <abbie@github.com>
2020-05-03 16:23:46 +0100

    options are malfunctioning

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const questionSection = document.getElementById('section-b');
 const startQuizBtn = document.getElementById('section-a');
 const eachQuestion = document.getElementById('question');
-const nextBtn = document.getElementsByClassName('next');
+const nextBtn = document.getElementById('next');
+const options = document.getElementById('main-quiz-container').children;
+const quizOverText1 = document.getElementById('over-text-1');
+const quizOverText2 = document.getElementById('over-text-2');
 //
 startQuizBtn.addEventListener('click', startQuiz);
-
-function restart() {
-    location.reload();
-}
 
 function startQuiz() {
     startQuizBtn.classList.add('hide');
     questionSection.classList.remove('hide');
     quizBody(questionsCounter);
+    
 }
+
+function restartQuiz() {
+    document.getElementById('quiz-over').classList.add('hide');
+    questionSection.classList.remove('hide');
+    nextBtn.classList.add('hide');
+    questionsCounter = 0;
+    quizBody(questionsCounter);
+}
+
+function restart() {
+    location.reload();
+}
+
 let question;
-let options;
+let option;
 function quizBody(index) {
 
 
@@ -23,10 +36,13 @@ function quizBody(index) {
     question = eachQuestion.innerHTML = questions[index].question;
 
     //displaying options
-    options = document.getElementById('0').innerHTML = 'A. ' + questions[index].options[0]
-    options = document.getElementById('1').innerHTML = 'B. ' + questions[index].options[1];
-    options = document.getElementById('2').innerHTML = 'C. ' + questions[index].options[2];
-    options = document.getElementById('3').innerHTML = 'D. ' + questions[index].options[3];
+    option = document.getElementById('0').innerHTML = 'A. ' + questions[index].options[0]
+    option = document.getElementById('1').innerHTML = 'B. ' + questions[index].options[1];
+    option = document.getElementById('2').innerHTML = 'C. ' + questions[index].options[2];
+    option = document.getElementById('3').innerHTML = 'D. ' + questions[index].options[3];
+
+    questionsAnswered = document.getElementById('q-c').innerHTML = questionsCounter + '/' + questions.length;
+    score = document.getElementById('s-c').innerHTML = 'Score: ' + scoreCounter;
 }
 
 let score;
@@ -37,18 +53,19 @@ let questionsCounter = 0;
 
 function choose(element) {
 
-    for (let i = 0; i < questions.length; i++) {
+    for (let i = 0; i <= questions.length; i++) {
         if (element.id == questions[i].answer) {
             element.classList.remove('options');
             element.classList.add('correct');
 
+
             document.getElementById(0).classList.add('disabled');
             document.getElementById(1).classList.add('disabled');
             document.getElementById(2).classList.add('disabled');
+            nextBtn.classList.remove('hide');
 
-            scoreCounter += 2;
-            score = document.getElementById('s-c').innerHTML = 'Total Score Obtained: ' + scoreCounter;
-
+            scoreCounter += 6;
+            score = document.getElementById('s-c').innerHTML = 'Score: ' + scoreCounter;
 
             break;
         } else {
@@ -61,28 +78,56 @@ function choose(element) {
 
             document.getElementById(2).classList.add('disabled');
 
-            document.getElementById(3).classList.remove('options');
-            document.getElementById(3).classList.add('correct');
+            document.getElementById(3).classList.add('disabled');
 
-            score = document.getElementById('s-c').innerHTML = 'Total Score Obtained: ' + scoreCounter;
+            nextBtn.classList.remove('hide');
+
+            score = document.getElementById('s-c').innerHTML = 'Score: ' + scoreCounter;
 
             break;
         }
     }
+    lockOptions();
 }
 
-function unlock() {
-    document.getElementById("next").style.display = "block";
-    nextBtn.disabled = false;
+function lockOptions() {
+    for (let i = 0; i < options.length; i++) {
+        options[i].classList.add('disabled');
+        if (options[i].id == questions[questionsCounter].answer) {
+            options[i].classList.add('correct');
+        }
+    }
 }
 
+function quizOver() {
+    questionSection.classList.add('hide');
+    
+    document.getElementById('quiz-over').classList.remove('hide');
+    if (scoreCounter <= 15) {
+    quizOverText1.innerHTML = 'Nice Try!';
+    quizOverText2.innerHTML = 'You Got ' + scoreCounter;
+    } else if (scoreCounter <= 25) {
+        quizOverText1.innerHTML = 'Brilliant!';
+        quizOverText2.innerHTML = 'You Got ' + scoreCounter;
+    } else {
+        quizOverText1.innerHTML = 'Just Like A Boss!';
+        quizOverText2.innerHTML = 'You Got ' + scoreCounter;}
+}
 function next() {
-        if (questionsCounter < questions.length - 1) {
+    if (questionsCounter == questions.length - 1) {
+        quizOver();
+        scoreCounter = 0;
+    }
+        else if (questionsCounter < questions.length - 1) {
+            
+            nextBtn.classList.add('hide');
             questionsCounter += 1;
-            questionsAnswered = document.getElementById('q-c').innerHTML = 'Total Questions Answered: ' + questionsCounter + '/' + questions.length;
             console.log(questionsCounter);
             quizBody(questionsCounter);
+            
         }
+
+        
 
     document.getElementById('0').classList.remove('correct', 'wrong', 'disabled');
     document.getElementById('0').classList.add('options');
@@ -101,53 +146,53 @@ function next() {
 //list of questions to display
 let questions = [
     {
-        question: 'What technology is used to make telephone calls over the Internet possible?',
+        question: 'What country won the very first FIFA World Cup in 1930?',
         options: [
-            'Bluetooth',
-            'Ethernet',
-            'NFC',
-            'VoIP'
+            'Italy',
+            'Spain',
+            'Uruguay',
+            'France'
+        ],
+        answer: 2
+    },
+    {
+        question: 'What year was the very first model of the iPhone released?',
+        options: [
+            '2001',
+            '2007',
+            '1998',
+            '2009'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Which email service is owned by Microsoft?',
+        options: [
+            'Yahoomail',
+            'Gmail',
+            'Webmail',
+            'Hotmail'
         ],
         answer: 3
     },
     {
-        question: 'How technology is used to make telephone calls over the Internet possible?',
+        question: 'What was Twitter’s original name?',
         options: [
-            'Bluetooth',
-            'Ethernet',
-            'NFC',
-            'VoIP'
+            'Twttr',
+            'Tweeter',
+            'The Tweet',
+            'Twitter'
         ],
-        answer: 3
+        answer: 0
     },
     {
-        question: 'Why technology is used to make telephone calls over the Internet possible?',
+        question: 'How many molecules of oxygen does ozone have?',
         options: [
-            'Bluetooth',
-            'Ethernet',
-            'NFC',
-            'VoIP'
+            '1',
+            '2',
+            '3',
+            '4'
         ],
-        answer: 3
-    },
-    {
-        question: 'When technology is used to make telephone calls over the Internet possible?',
-        options: [
-            'Bluetooth',
-            'Ethernet',
-            'NFC',
-            'VoIP'
-        ],
-        answer: 3
-    },
-    {
-        question: 'Could technology is used to make telephone calls over the Internet possible?',
-        options: [
-            'Bluetooth',
-            'Ethernet',
-            'NFC',
-            'VoIP'
-        ],
-        answer: 3
+        answer: 2
     },
 ]
